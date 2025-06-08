e826753eb5cee597abab8806d822b0d1694ff067
Josh Nolan <joshynolan02@gmail.com>
2025-06-08 02:02:48 +0100

    Sample file

---
+++ Ch.1/Sample.cpp
@@ -0,0 +1,14 @@
+/* 
+This is a simple C++ program.
+*/
+
+#include <iostream>
+using namespace std;
+
+// A C++ program begins at main().
+int main()
+{
+    cout << "C++ is power programming.";
+
+    return 0;
+}
