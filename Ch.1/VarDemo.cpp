625e314d003f5c611e0870f3e5b48ad8bf78dc21
Josh Nolan <joshynolan02@gmail.com>
2025-06-08 02:03:08 +0100

    Variable demo file

---
+++ Ch.1/VarDemo.cpp
@@ -0,0 +1,17 @@
+// Using a variable. 
+// Variable = a named memory location that can be assigned a changeable value.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int length; // this declares a variable
+
+    length = 33000; // this assigns the value 7 to length
+
+    cout << "The length is ";
+    cout << length; // This displays 7
+
+    return 0;
+}
