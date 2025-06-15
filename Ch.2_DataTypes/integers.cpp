42b7e7bf77e2a79ff8e78011b343f74fbb355c92
Josh Nolan <joshynolan02@gmail.com>
2025-06-15 17:44:32 +0100

    post-testing

--- Ch.2_DataTypes/integers.cpp
+++ Ch.2_DataTypes/integers.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 int main()
 {
-    short int i; // 2 bytes. A signed short int short unsigned
-    int j; // 4 bytes. An unsigned short integer.
+    short int i; // 2 bytes. A signed short integer. -32768 to 32767
+    int j; // 4 bytes. An unsigned short integer. 0 to 65535
     j = 60000; // 60000 is too large for a short int, so it will be truncated to -5536
     i = j; // 2 bytes  
     cout << i << " " << j;
